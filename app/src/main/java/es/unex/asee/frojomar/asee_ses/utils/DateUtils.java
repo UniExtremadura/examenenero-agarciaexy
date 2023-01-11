a221b2d78261d3549a11f67a2c54a6cb14a60b65
Alonso <agarciaexy@alumnos.unex.es>
2023-01-11 19:35:52 +0100

    Simulacion de la implementacion del caso de uso 21 - Implementacion - Implements components - DS #TIME 1d 4h

--- app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
+++ app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
@@ -17,6 +17,8 @@ public class DateUtils {
     //Simulacion de la implementacion del caso de uso 18 - Integracion y test - Integrate each subsistem - AS
     //Simulacion de la implementacion del caso de uso 18 - Integracion y test - Test and evaluate - AS
     //Simulacion de la implementacion del caso de uso 18 - Integracion y test - Test and evaluate - DJ
+    //Simulacion de la implementacion del caso de uso 21 - Implementacion - Implements components - AS
+    //Simulacion de la implementacion del caso de uso 21 - Implementacion - Implements components - DS
 
     //private static final String FORMAT_DATE="dd-MMM-yyyy,hh:mm aa";
     private static final String FORMAT_TIME = "HH:mm:ss:SSS";
